5a731db58a7f7e4c74b3c41476f12fa53e532e1e
kristina_lengvinayte <klengvinayte@gmail.com>
2023-01-17 22:02:35 +0200

    added views & controllers

--- Gemfile
+++ Gemfile
@@ -4,10 +4,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.2"
 
 gem "rails", "~> 7.0.4"
+gem 'rails_admin'
 
 gem "sprockets-rails"
-
-gem "sqlite3", "~> 1.4"
 
 gem "puma", "~> 5.0"
 
@@ -21,13 +20,18 @@ gem "cssbundling-rails"
 gem 'devise'
 gem 'devise-i18n'
 gem "jbuilder"
+gem 'jquery-rails'
 
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 gem "bootsnap", require: false
 
 group :development, :test do
+  gem "sqlite3", "~> 1.4"
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'rspec-rails', '~> 3.4'
+  gem 'shoulda-matchers'
+  gem 'launchy'
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery with: :exception
+
+  before_action :configure_devise_params, if: :devise_controller?
+
+  before_action :set_new_game
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password) }
+  end
+
+  # Болванка новой игры для кнопки "Начать игру", доступной на любой странице сайта
+  def set_new_game
+    @new_game ||= Game.new
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
