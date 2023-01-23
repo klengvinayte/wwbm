55c7b18a7a7c3f49ef1110bdc5c559daf258013c
kristina_lengvinayte <klengvinayte@gmail.com>
2023-01-23 21:16:23 +0200

    added tests to games_controller2

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -18,6 +18,38 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
       expect(flash[:alert]).to be
     end
+
+    it "kick from #create" do
+      post :create
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it "kick from #answer" do
+      put :answer, params: { id: game_w_questions.id }
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it "kick from #take_money" do
+      put :take_money, params: { id: game_w_questions.id }
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it "kick from #help" do
+      put :help, params: { id: game_w_questions.id }
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
   end
 
   context "Usual user" do
@@ -58,7 +90,18 @@ RSpec.describe GamesController, type: :controller do
       expect(game.current_level).to be > 0
       expect(response).to redirect_to game_path(game)
       expect(flash.empty?).to be_truthy
+    end
 
+    it "answer incorrect" do
+      put :answer, params: { id: game_w_questions.id, letter: "a" }
+
+      game = assigns(:game)
+
+      expect(game.finished?).to be_truthy
+      expect(game.current_level).to be 0
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:alert]).to be
+      expect(game.prize).to be 0
     end
 
     it "dont show another game" do
