6e46237d836dc6c5c7ed06d3dfae8fd088d7e383
kristina_lengvinayte <klengvinayte@gmail.com>
2023-01-30 23:19:31 +0200

    Integration tests added2

--- spec/features/user_views_other_profile_spec.rb
+++ spec/features/user_views_other_profile_spec.rb
@@ -1,22 +1,24 @@
 require "rails_helper"
 
 RSpec.feature "USER views another profile", type: :feature do
-
   let(:game_user) { create :user, name: "Kris" }
 
   let!(:games) do
     [
-      create(:game, user: game_user,
-             current_level: 5,
-             prize: 1000,
-             created_at: "2023-01-28, 14:39 UTC".to_datetime,
-             finished_at: "2023-01-28, 14:42 UTC".to_datetime
+      create(
+        :game, user: game_user,
+        is_failed: true,
+        current_level: 6,
+        prize: 1000,
+        created_at: "2023-01-28, 14:39 UTC".to_datetime,
+        finished_at: "2023-01-28, 14:42 UTC".to_datetime
       ),
-      create(:game, user: game_user,
-             current_level: 15,
-             prize: 1000000,
-             created_at: "2023-01-30, 08:01 UTC".to_datetime,
-             finished_at: "2023-01-30, 08:07 UTC".to_datetime
+      create(
+        :game, user: game_user,
+        current_level: 15,
+        prize: 1000000,
+        created_at: "2023-01-30, 08:01 UTC".to_datetime,
+        finished_at: "2023-01-30, 08:07 UTC".to_datetime
       )
     ]
   end
@@ -30,7 +32,7 @@ RSpec.feature "USER views another profile", type: :feature do
     expect(page).to have_content("30 янв., 08:01")
 
     expect(page).to have_content("победа")
-    expect(page).to have_content("деньги")
+    expect(page).to have_content("проигрыш")
 
     expect(page).to have_content("1 000 000 ₽")
     expect(page).to have_content("1 000 ₽")
@@ -38,7 +40,7 @@ RSpec.feature "USER views another profile", type: :feature do
     expect(page).to have_content("50/50 phone users")
 
     expect(page).to have_content("15")
-    expect(page).to have_content("5")
+    expect(page).to have_content("6")
 
     expect(page).not_to have_content("Сменить имя и пароль")
   end
